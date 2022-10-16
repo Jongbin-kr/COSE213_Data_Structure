febc96fe6b7135d77e0782f62d42535170752126
Jongbin Won <wonjb001@naver.com>
2022-10-17 01:13:06 +0900

    Edit filename and Add type_conversion.c

---
+++ ch1~2_C_review/29p_type_conversion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int x;
+    float y = 3.0;
+    x = y;
+    
+    printf("%f\n", y);
+    printf("%d\n", x);
+
+
+
+    return 0;
+}
---
+++ ch1~2_C_review/37p_switch_case_weekday_example.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+    int input;
+    printf("user intput: ");
+    scanf("%d", &input);
+
+    switch(input){
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+    }
+
+
+
+    return 0;
+}
